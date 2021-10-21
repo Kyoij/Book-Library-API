bb8036c04b76fa9318c530034bea4aa0f0c8f2f8
Tuan Vu <tuanvundvn@gmail.com>
2021-10-21 20:54:36 +0700

    add readme

--- src/config/index.config.ts
+++ src/config/index.config.ts
@@ -1,7 +1,7 @@
-import { config } from "dotenv";
+import { config } from 'dotenv';
 
 config();
 
-export const port = Number(process.env.PORT);
+export const port = Number(process.env.PORT) || 80;
 export const mongo_url = process.env.MONGO_URL as string;
 export const secret = process.env.SECRET as string;
